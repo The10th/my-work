6568801032a611d534e401a38e5b906b2d5612c8
Julian Nguyen <julian.hv.nguyen@gmail.com>
2014-10-22 10:28:41 -0700

    jquery/modal fix

--- js/js.js
+++ js/js.js
@@ -11,12 +11,26 @@ function random_color()
             return color;
         }
 
+        // on page load creates sample secrets
 
- $(document).ready(function(){
-        
+        $(document).ready(function(){
+
+            for (var i = 0; i <= 8; i++) {
+               $('.fussy').append("<div class='col-md-3 secbox'><p id='awesome'>Photo booth fixie pour-over Cosby sweater meh flexitarian brunch, kogi High Life. Plaid Cosby sweater asymmetrical gluten-free bicycle rights, selfies tofu before they sold out wolf. XOXO heirloom cornhole, polaroid kogi ennui health goth disrupt next level flexitarian seitan Portland</p></div>")
+           };
+
         //initialize Stellar.js
-         $(window).stellar();
-        
+        $(window).stellar();
+
+         //modal control - expands modal size from 0 width 0 height:
+
+         $('.mainModal').click(function(event){
+
+            $('.modalDialog>div').css('width',750)
+            $('.modalDialog>div').css('height',850)
+            event.stopPropagation();
+        });
+
         //color click logic
         $('#large_box').click(function(){
             $('#large_box').css('background-color',random_color()).children().css('background-color',random_color());
@@ -33,7 +47,7 @@ function random_color()
             $('#large_box').css('background-color',random_color());
         });
 
-         var inside = $('.secla').innerHeight();
+        var inside = $('.secla').innerHeight();
         var count = 0;
         var divs = 0;
 
@@ -42,7 +56,8 @@ function random_color()
             if (!('maxLength' in el)) {
                 var max = el.attributes.maxLength.value;
                 el.keyup = function () {
-                    if (this.value.length >= max) return false;
+                    if (this.value.length >= max) 
+                        return false;
                 };
             }
         }
@@ -57,7 +72,7 @@ function random_color()
         }
 
         $(secs).click(function(){
-            
+
             function calculateLIsInRow() {
 
 
@@ -90,10 +105,12 @@ function random_color()
             var remaining = 150 - $('#texs').val().length;
             $('.countdown').text(remaining + ' characters remaining')
             $('#secs').prop('disabled', true);
-            return false;
+            event.preventDefault();
         })
 
-        $('.secla').scroll(function(){
+    // removes div content on scroll
+
+    $('.secla').scroll(function(){
 
             //109 is $('.secla').height()
             $('.secla').scrollTop() >= $('.secla').height()+100;
@@ -110,24 +127,45 @@ function random_color()
         });
 
 
-        $('#secrets').on('click' , function() {
-            if( $.trim($('#texs').val()).length > 0 ){
-                $('#secs').prop('disabled', false);
-            }
-            else {
-                $('#secs').prop('disabled', true);
-            }
+    $('#secrets').on('click' , function() {
+        if( $.trim($('#texs').val()).length > 0 ){
+            $('#secs').prop('disabled', false);
+        }
+        else {
+            $('#secs').prop('disabled', true);
+        }
 
-        });
+    });
 
-        updateCountdown();
-        $('#texs').change(updateCountdown);
-        $('#texs').keyup(updateCountdown);
+    updateCountdown();
+    $('#texs').change(updateCountdown);
+    $('#texs').keyup(updateCountdown);
 
 
-        $('#refresh').click(function() {
-            location.reload();
-        });
+    $('#refresh').click(function() {
+        location.reload();
+    });
+
+ // contact icon spin   
+
+ var $spindoc = $('.spindoc'),
+ $body = $(document.body),
+ bodyHeight = $body.height();
+
+ $(window).scroll(function () {
+    $spindoc.css({
+        'transform': 'rotate(' + ($body.scrollTop() / bodyHeight * 715) + 'deg)'
+    });
+});
+ var $revspin = $('.revspin'),
+ $body = $(document.body),
+ bodyHeight = $body.height();
+
+ $(window).scroll(function () {
+    $revspin.css({
+        'transform': 'rotate(' + ($body.scrollTop() / bodyHeight * -715) + 'deg)'
+    });
+});
 });
 
     
