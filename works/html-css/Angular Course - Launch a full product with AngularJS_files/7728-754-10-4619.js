422ca129ecf741f8024ba66c5909fd61387adcc7
Julian Nguyen <julian.hv.nguyen@gmail.com>
2014-10-24 17:28:02 -0700

    demo day finish

---
+++ works/html-css/Angular Course - Launch a full product with AngularJS_files/7728-754-10-4619.js
@@ -0,0 +1,15 @@
+
+      (function(){
+        
+
+        var isNewVersion = olark._ && olark._.versions && (olark._.versions.follow || olark._.versions.popout)
+        if(isNewVersion) {
+          olark._.finish({"system":{"hashchange_events_trigger_page_change":0,"show_popout":0,"allow_change_colors":true,"disable_default_visitor_information":0,"email_body_error_text":"You must complete all fields and specify a valid email address","operator_has_stopped_typing_text":"has stopped typing","require_offline_phone":0,"offline_message":"Don't worry, we're fast and try to respond to all questions the same day!","hide_not_available":0,"show_pre_chat":0,"allow_change_width":true,"allow_change_height":true,"allow_mobile_boot":0,"habla_offline_sent_text":"Thanks for your message!  We'll get back to you shortly.","habla_name_input_text":"\u003Cclick here\u003E and type your Name","say_text":"Type here and hit \u003Center\u003E to chat","allowed_domains":"","habla_offline_email_text":"\u003Cclick here\u003E and type your Email","start_expanded":0,"inline_css_url":"static.olark.com/css/a/f/afa97804fc7dceb9cd203903b56a41ea.css","bottom_margin":0,"right_margin":20,"disable_extra_br":true,"close_hides_window":0,"habla_offline_body_text":"Leave a note","expandOnMessageReceived":0,"expandOnFirstMessageReceived":1,"not_available_text":"Question?","welcome_msg":"What can I help you with?","top_margin":0,"disable_width":true,"habla_offline_phone_text":"click here and type your Phone","ended_chat_message":"This chat has ended, start typing below if you need anything else!","start_hidden":0,"offline_msg_mode":1,"inline_css_url_ie":"static.olark.com/css/b/e/be3bbc398adcafb16095cba7c2146379.css","inline_css_url_quirks":"static.olark.com/css/6/8/685f02893bce2b9ba1fcecbe5e8e4a0a.css","height":150,"disable_offline_messaging_fallback":true,"in_chat_text":"Session now in-progress","habla_offline_submit_value":"Send","operator_is_typing_text":"is typing...","before_chat_text":"Take a break and say hi!","disableJSStyles":false,"width":300,"right_to_left":false,"corner_position":"BR","left_margin":20,"show_in_buddy_list":"all","hkey":"PHNwYW4gc3R5bGU9ImRpc3BsYXk6bm9uZSI+PGEgaWQ9ImhibGluazkiPjwvYT5odHRwOi8vd3d3Lm9sYXJrLmNvbTwvc3Bhbj48YSBocmVmPSJodHRwOi8vd3d3Lm9sYXJrLmNvbS8/cmlkPTc3MjgtNzU0LTEwLTQ2MTkmYW1wO2J1c2luZXNzPTEmYW1wO3V0bV9tZWRpdW09d2lkZ2V0JmFtcDt1dG1fY2FtcGFpZ249ZnJlZV9idXNpbmVzc19jaGF0JmFtcDt1dG1fc291cmNlPTc3MjgtNzU0LTEwLTQ2MTkiIGlkPSJoYmxpbms5OSIgdGFyZ2V0PSJfYmxhbmsiPkZyZWUgT2xhcmsgQ2hhdCBmb3IgQnVzaW5lc3M8L2E+","md5":"40dbd3803c6fe81fb53c188674380980","site_id":"7728-754-10-4619","template":"azul","operators":{}}});
+        }else{
+          olark.configure(function(conf){
+            conf.system.site_id="7728-754-10-4619";
+          });
+          olark._.finish();
+        }
+      })();
+    
